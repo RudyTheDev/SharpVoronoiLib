c11f1a53f7f399be343d3c5deebac002fe3327f6
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2025-05-21 14:30:07 +0300

    To file-scoped namespaces for KD tree

--- Supercluster.KDTree/Supercluster.KDTree/Utilities/BinaryTreeNavigation.cs
+++ Supercluster.KDTree/Supercluster.KDTree/Utilities/BinaryTreeNavigation.cs
@@ -2,46 +2,45 @@
 // Copyright (c) Eric Regina. All rights reserved.
 // </copyright>
 
-namespace Supercluster.KDTree.Utilities
+namespace Supercluster.KDTree.Utilities;
+
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Contains methods used for doing index arithmetic to traverse nodes in a binary tree.
+/// </summary>
+public static class BinaryTreeNavigation
 {
-    using System.Runtime.CompilerServices;
+    /// <summary>
+    /// Computes the index of the right child of the current node-index.
+    /// </summary>
+    /// <param name="index">The index of the current node.</param>
+    /// <returns>The index of the right child.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int RightChildIndex(int index)
+    {
+        return (2 * index) + 2;
+    }
 
     /// <summary>
-    /// Contains methods used for doing index arithmetic to traverse nodes in a binary tree.
+    /// Computes the index of the left child of the current node-index.
     /// </summary>
-    public static class BinaryTreeNavigation
+    /// <param name="index">The index of the current node.</param>
+    /// <returns>The index of the left child.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int LeftChildIndex(int index)
     {
-        /// <summary>
-        /// Computes the index of the right child of the current node-index.
-        /// </summary>
-        /// <param name="index">The index of the current node.</param>
-        /// <returns>The index of the right child.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int RightChildIndex(int index)
-        {
-            return (2 * index) + 2;
-        }
+        return (2 * index) + 1;
+    }
 
-        /// <summary>
-        /// Computes the index of the left child of the current node-index.
-        /// </summary>
-        /// <param name="index">The index of the current node.</param>
-        /// <returns>The index of the left child.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int LeftChildIndex(int index)
-        {
-            return (2 * index) + 1;
-        }
-
-        /// <summary>
-        /// Computes the index of the parent of the current node-index.
-        /// </summary>
-        /// <param name="index">The index of the current node.</param>
-        /// <returns>The index of the parent node.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int ParentIndex(int index)
-        {
-            return (index - 1) / 2;
-        }
+    /// <summary>
+    /// Computes the index of the parent of the current node-index.
+    /// </summary>
+    /// <param name="index">The index of the current node.</param>
+    /// <returns>The index of the parent node.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int ParentIndex(int index)
+    {
+        return (index - 1) / 2;
     }
 }
