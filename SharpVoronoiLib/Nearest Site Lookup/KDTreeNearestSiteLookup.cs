7731682ce8642b87cbb30ee3c3a75f7a9ee98ebc
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2025-05-21 15:34:40 +0300

    Wire KD-tree nearest site lookup

--- SharpVoronoiLib/Nearest Site Lookup/KDTreeNearestSiteLookup.cs
+++ SharpVoronoiLib/Nearest Site Lookup/KDTreeNearestSiteLookup.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using Supercluster.KDTree;
 
 namespace SharpVoronoiLib;
 
 public class KDTreeNearestSiteLookup : INearestSiteLookup
 {
+    private int _lastVersion = -1;
+    
+    private KDTree<VoronoiSite> _kdTree = null!;
+    
+    
     public VoronoiSite GetNearestSiteTo(List<VoronoiSite> sites, double x, double y, int version)
     {
-        throw new NotImplementedException();
+        if (version != _lastVersion)
+            _kdTree = new KDTree<VoronoiSite>(PointsFromSites(sites), sites.ToArray());
+        
+        _lastVersion = version;
+        
+        Tuple<double[], VoronoiSite>[] nearest = _kdTree.NearestNeighbors([ x, y ], 1);
+
+        return nearest[0].Item2;
+    }
+
+    
+    private static double[][] PointsFromSites(List<VoronoiSite> sites)
+    {
+        double[][] points = new double[sites.Count][];
+
+        for (int i = 0; i < sites.Count; i++)
+            points[i] = [ sites[i].X, sites[i].Y ];
+
+        return points;
     }
 }
