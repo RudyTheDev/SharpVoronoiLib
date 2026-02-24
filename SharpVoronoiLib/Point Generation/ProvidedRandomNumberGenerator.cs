48f901ed554997d7525f7f5d270f122dcc3032ab
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2026-02-24 14:42:36 +0200

    Add rng variant for a given random instance in case user wants their own persistent one

---
+++ SharpVoronoiLib/Point Generation/ProvidedRandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace SharpVoronoiLib;
+
+[PublicAPI]
+public class ProvidedRandomNumberGenerator : IRandomNumberGenerator
+{
+    private readonly Random _random;
+    
+    
+    public ProvidedRandomNumberGenerator(Random random)
+    {
+        _random = random;
+    }
+    
+    
+    public double NextDouble() => _random.NextDouble();
+}
